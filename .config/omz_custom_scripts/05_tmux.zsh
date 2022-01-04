cfcbd85600fdb9f82a2648c247c914730b072792
Benedikt Engeser <beneeng@Benedikts-MBP.home>
2022-01-04 20:49:02 +0100

    change color scheme and tmux conditions

--- .config/omz_custom_scripts/05_tmux.zsh
+++ .config/omz_custom_scripts/05_tmux.zsh
@@ -1,7 +1,7 @@
 if ! has_tool tmux; then
         return 0
 fi
-if ! [ -n "SSH_CONNECTION" ] &&  ! [[ $TERM == "screen-256color" ]]; then
+if ! [ -z "$SSH_CONNECTION" ] &&  ! [[ $TERM == "screen-256color" ]]; then
         exec sh -c "tmux a || tmux || zsh"
 fi
 
