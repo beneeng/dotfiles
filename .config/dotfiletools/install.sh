7c51e64a5aad1e65ef85125da13360dce97c1056
Benedikt Engeser <beneeng@Benedikts-MBP.home>
2022-01-04 18:38:35 +0100

    add install script

---
+++ .config/dotfiletools/install.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+
+function setup_mac(){
+    brew install vim
+    brew install fzf
+    $(brew --prefix)/opt/fzf/install
+    brew install direnv
+}
+
+function setup_ubuntu(){
+    sudo apt-get install vim fzf direnv
+    $(brew --prefix)/opt/fzf/install
+}
+
+
+
+CONFIG_FOLDER="$HOME/.cfg/"
+GIT_REPO="git@github.com:beneeng/dotfiles.git"
+
+command -v git &> /dev/null || { echo "install git first"; exit 1 }
+
+alias config='git --git-dir=$CONFIG_FOLDER --work-tree=$HOME'
+
+[ -z $(grep "^.cfg$" .gitignore 2>/dev/null) ] && echo ".cfg" >> .gitignore
+
+git clone --bare $GIT_REPO $CONFIG_FOLDER
+
+#checkout config
+config checkout
+
+# remove untracked files in home directory from `config status`
+config config --local status.showUntrackedFiles no
+
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        setup_ubuntu
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        setup_mac
+fi
+
+export KEEP_ZSHRC='yes'
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
